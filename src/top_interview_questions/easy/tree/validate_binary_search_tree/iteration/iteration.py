72aa21d7493e99b45eee2427b651e6430c5ab275
Lillard Kuo <lillard.kuo@lileesystems>
2025-06-21 23:27:52 +0800

    Add the iteration approach for the  "validate binary search tree" problem.

---
+++ src/top_interview_questions/easy/tree/validate_binary_search_tree/iteration/iteration.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+import math
+from typing import Optional
+
+from ...model import TreeNode
+
+
+@dataclass
+class QueueItem:
+    node: TreeNode | None
+    lower_bound: float
+    upper_bound: float
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        queue: list[QueueItem] = [
+            QueueItem(
+                node=root,
+                lower_bound=-math.inf,
+                upper_bound=math.inf,
+            )
+        ]
+
+        while queue:
+            item = queue.pop()
+            if not item.node:
+                continue
+
+            node = item.node
+            lower_bound = item.lower_bound
+            upper_bound = item.upper_bound
+
+            if node.val <= lower_bound or node.val >= upper_bound:
+                return False
+
+            queue.append(
+                QueueItem(
+                    node=node.left,
+                    lower_bound=lower_bound,
+                    upper_bound=node.val,
+                )
+            )
+            queue.append(
+                QueueItem(
+                    node=node.right,
+                    lower_bound=node.val,
+                    upper_bound=upper_bound,
+                )
+            )
+
+        return True
