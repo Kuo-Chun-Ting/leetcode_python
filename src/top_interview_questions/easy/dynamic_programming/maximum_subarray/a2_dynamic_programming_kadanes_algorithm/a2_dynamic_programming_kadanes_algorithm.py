c3f94bc3f246f2d8f1826d6c694d3ced35fb6802
Lillard Kuo <lillard.kuo@lileesystems>
2025-08-16 14:44:40 +0800

    Solve the "Maximum Subarray" problem.

---
+++ src/top_interview_questions/easy/dynamic_programming/maximum_subarray/a2_dynamic_programming_kadanes_algorithm/a2_dynamic_programming_kadanes_algorithm.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            if dp[i - 1] < dp[i]:
+                dp[i] = nums[i]
+            else:
+                dp[i] = dp[i - 1] + nums[i]
+
+        return max(dp)
